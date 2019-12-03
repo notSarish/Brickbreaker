84e85c40aeabc00c8869e59b1fef2a4365998d0f
Michael Sun <msun2019@pingry.org>
2019-12-02 22:12:57 -0600

    Added button handler for normal mode, working on getting MotionLayout working

--- app/src/main/java/com/example/brickbreaker/MainActivity.java
+++ app/src/main/java/com/example/brickbreaker/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.brickbreaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button normal = (Button) findViewById(R.id.playNormal);
+        View.OnClickListener handler = new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent normalIntent = new Intent(MainActivity.this, NormalModeActivity.class);
+                MainActivity.this.startActivity(normalIntent);
+            }
+        };
+        normal.setOnClickListener(handler);
     }
+//
+//    private void normalModeClickHandler() {
+//        Intent normalIntent = new Intent(MainActivity.this, NormalMode.class);
+//        MainActivity.this.startActivity(normalIntent);
+//    }
 }
