9456c3616897a99c945c6fd643d7d2f3e0d5ce67
notSarish <35690963+notSarish@users.noreply.github.com>
2019-12-06 00:57:44 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/brickbreaker/MainActivity.java
+++ app/src/main/java/com/example/brickbreaker/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
+        //buttons
         super.onCreate(savedInstanceState);
         drawView = new DrawView(this);
         drawView.setBackgroundColor(Color.WHITE);
@@ -40,10 +40,21 @@ public class MainActivity extends Activity {
     public boolean onTouchEvent(MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
+        //what happens if you touch the first button
         if (x >= 0 && x <= 1500 && y >= 200 && y <= 350) {
           //  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
            // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
             setContentView(new GamePanel(this));
+            return true;
+        }
+        //what happens if you touch the second button
+        if (x >= 0 && x <= 1500 && y >= 375 && y <= 525) {
+            //setContentView to the self made design game
+            return true;
+        }
+
+        if (x >= 0 && x <= 1500 && y >= 550 && y <= 700) {
+            //setContentView to the make your own design page
             return true;
         }
         return false;
