7fa796ee34b58af742d2d88a1990a2e32cfd5202
notSarish <35690963+notSarish@users.noreply.github.com>
2019-12-08 19:52:03 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/brickbreaker/MainActivity.java
+++ app/src/main/java/com/example/brickbreaker/MainActivity.java
@@ -1,4 +1,5 @@
-package com.example.brickbreaker;
+package com.example.myapplication;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -14,6 +15,8 @@ import android.widget.Button;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Color;
+import android.widget.RelativeLayout;
+import android.widget.ViewFlipper;
 
 
 import static android.view.Window.FEATURE_NO_TITLE;
@@ -24,6 +27,8 @@ public class MainActivity extends Activity {
     private Canvas canvas = new Canvas();
     private Color color = new Color();
     DrawView drawView;
+    GamePanel b; //= new GamePanel(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -31,7 +36,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         drawView = new DrawView(this);
         drawView.setBackgroundColor(Color.WHITE);
+       // b = new GamePanel(this);
+
+//        ViewFlipper flipper = drawView;
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //RelativeLayout a = new DrawView(this);
+        //DrawView a = new DrawView(this);
+        //b.setVisibility(View.GONE);
+        //drawView.setVisibility(View.VISIBLE);
         setContentView(drawView);
+
 
 
 
@@ -42,22 +58,30 @@ public class MainActivity extends Activity {
         float x = event.getX();
         float y = event.getY();
         //what happens if you touch the first button
-        if (x >= 0 && x <= 1500 && y >= 200 && y <= 350) {
+        if (drawView.getVisibility() != View.GONE && x >= 0 && x <= 1500 && y >= 200 && y <= 350) {
           //  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
            // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+           // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+           // b = new GamePanel(this);
+            //drawView.setVisibility(View.GONE);
+            //b.setVisibility(View.VISIBLE);
             setContentView(new GamePanel(this));
+            drawView.setVisibility(View.GONE);
+         //   drawView = null;
             return true;
         }
         //what happens if you touch the second button
-        if (x >= 0 && x <= 1500 && y >= 375 && y <= 525) {
+       /* if (drawView.getVisibility() != View.GONE && x >= 0 && x <= 1500 && y >= 375 && y <= 525) {
             //setContentView to the self made design game
+
             return true;
         }
 
-        if (x >= 0 && x <= 1500 && y >= 550 && y <= 700) {
+        if (drawView.getVisibility() != View.GONE && x >= 0 && x <= 1500 && y >= 550 && y <= 700) {
             //setContentView to the make your own design page
             return true;
-        }
+        }*/
         return false;
     }
 
