26b333da7a217a85292709a22eff98cea480c857
notSarish <35690963+notSarish@users.noreply.github.com>
2019-12-09 20:16:08 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/brickbreaker/MainActivity.java
+++ app/src/main/java/com/example/brickbreaker/MainActivity.java
@@ -19,9 +19,6 @@ import android.widget.RelativeLayout;
 import android.widget.ViewFlipper;
 
 
-import com.example.brickbreaker.DrawView;
-import com.example.brickbreaker.GamePanel;
-
 import static android.view.Window.FEATURE_NO_TITLE;
 //import android.widget.Button;
 //import android.widget.LinearLayout;
@@ -30,7 +27,13 @@ public class MainActivity extends Activity {
     private Canvas canvas = new Canvas();
     private Color color = new Color();
     DrawView drawView;
+    drawView2 drawview2;
     GamePanel b; //= new GamePanel(this);
+    boolean isItPlaying = false;
+    boolean weInCustomMake = false;
+    boolean inNormalMode = false;
+    Bricks[][] a;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +45,7 @@ public class MainActivity extends Activity {
        // b = new GamePanel(this);
 
 //        ViewFlipper flipper = drawView;
-
+       // b.setVisibility(View.GONE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //RelativeLayout a = new DrawView(this);
@@ -61,19 +64,12 @@ public class MainActivity extends Activity {
         float x = event.getX();
         float y = event.getY();
         //what happens if you touch the first button
-        if (drawView.getVisibility() != View.GONE && x >= 0 && x <= 1500 && y >= 200 && y <= 350) {
-          //  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-           // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-           // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-           // b = new GamePanel(this);
-            //drawView.setVisibility(View.GONE);
-            //b.setVisibility(View.VISIBLE);
-            setContentView(new GamePanel(this));
-            drawView.setVisibility(View.GONE);
-         //   drawView = null;
-            return true;
-        }
+
+       // if (inNormalMode) {
+            //if within the boundaries of the back button
+            //setContentView(drawView)
+         //   inNormalMode = false;
+        //}
         //what happens if you touch the second button
        /* if (drawView.getVisibility() != View.GONE && x >= 0 && x <= 1500 && y >= 375 && y <= 525) {
             //setContentView to the self made design game
@@ -81,10 +77,45 @@ public class MainActivity extends Activity {
             return true;
         }
 
-        if (drawView.getVisibility() != View.GONE && x >= 0 && x <= 1500 && y >= 550 && y <= 700) {
-            //setContentView to the make your own design page
+
+*/      if (weInCustomMake && !inNormalMode) { //&& x >= 0 && x<=1500 && y >= 0 && y <= 875) {
+            //keep adding 1 to the color for each touch
+            //badda bing
+
+            int xordinate = (int) x/270;
+            int yCoordinate = (int) y/175;
+            //if (x < 4 && y < 5) {
+
+            int col = a[xordinate][yCoordinate].getColor();
+            a[xordinate][yCoordinate].setColor(col);
+            //}
+            drawview2 = new drawView2(this, a);
+            setContentView(drawview2);
             return true;
-        }*/
+            //have a back statement to get out
+        }
+        if (!weInCustomMake && drawView.getVisibility() != View.GONE && !inNormalMode && x >= 0 && x <= 1500 && y >= 550 && y <= 700) {
+            //setContentView to the make your own design page
+            a = new Bricks[5][4];
+            drawview2 = new drawView2(this, a);
+            setContentView(drawview2);
+            drawView.setVisibility(View.GONE);
+            weInCustomMake = true;
+
+            return true;
+        }
+        
+        //
+        if (!weInCustomMake && drawView.getVisibility() != View.GONE && x >= 0 && x <= 1500 && y >= 200 && y <= 350) {
+
+
+            setContentView(new GamePanel(this));
+            drawView.setVisibility(View.GONE);
+            inNormalMode = true;
+
+            return true;
+        }
+
         return false;
     }
 
