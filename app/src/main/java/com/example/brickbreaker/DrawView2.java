ca47205ee9e31e0a18e73a0c918dda456faf2159
notSarish <35690963+notSarish@users.noreply.github.com>
2019-12-09 23:37:41 -0600

    small changes

--- app/src/main/java/com/example/brickbreaker/DrawView2.java
+++ app/src/main/java/com/example/brickbreaker/DrawView2.java
@@ -9,13 +9,13 @@ import android.view.View;
 
 
 
-public class DrawView2 extends View {
+public class drawView2 extends View {
     private Paint paint = new Paint();
     private Bricks[][] rectss;
     private Paint paint0 = new Paint();
     private Canvas canvass = new Canvas();
     private boolean condition;// = true;
-    public DrawView2(Context context, Bricks[][] rects, Boolean set) {
+    public drawView2(Context context, Bricks[][] rects, Boolean set) {
         super(context);
         int color;
         for (int i = 0; i < rects.length; i++) {
@@ -76,60 +76,7 @@ public class DrawView2 extends View {
             paint0.setTextSize(60);
             paint0.setColor(Color.WHITE);
             paint0.setStyle(Paint.Style.FILL_AND_STROKE);
-            canvas.drawText("Save Level and Exit", 300, 1650, paint0);
-
-        } else {
-            Paint paint0 = new Paint();
-            Paint paint = new Paint();
-            Paint paint2 = new Paint();
-            Paint paint3 = new Paint();
-            Rect rect0 = new Rect(0, 0, 1500, 175);
-            Rect rect = new Rect(0, 200, 1500, 350);
-            Rect rect2 = new Rect(0, 375, 1500, 525);
-            Rect rect3 = new Rect(0,550, 1500, 700);
-            paint0.setStyle(Paint.Style.FILL);
-            paint0.setColor(Color.BLUE);
-            canvass.drawRect(rect0, paint0);
-            //paint0.setStyle(Paint.Style.STROKE);
-            //paint0.setColor(Color.BLACK);
-            //canvass.drawRect(rect, paint0);
-            paint0.setTextSize(60);
-            paint0.setColor(Color.WHITE);
-            paint0.setStyle(Paint.Style.FILL_AND_STROKE);
-            canvass.drawText("Brick Breaker", 347, 115, paint0);
-
-
-            //first button
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.WHITE);
-            canvass.drawRect(rect, paint);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.BLACK);
-            canvass.drawRect(rect, paint);
-            paint.setTextSize(55);
-            paint.setStyle(Paint.Style.FILL_AND_STROKE);
-            canvass.drawText("Play Normal Mode", 300, 300, paint);
-
-            //second button
-            paint2.setStyle(Paint.Style.FILL);
-            paint2.setColor(Color.WHITE);
-            canvass.drawRect(rect2, paint2);
-            paint2.setStyle(Paint.Style.STROKE);
-            paint2.setColor(Color.BLACK);
-            canvass.drawRect(rect2, paint2);
-            paint2.setTextSize(55);
-            paint2.setStyle(Paint.Style.FILL_AND_STROKE);
-            canvass.drawText("Play Your Own Design", 265, 475, paint2);
-
-            paint3.setStyle(Paint.Style.FILL);
-            paint3.setColor(Color.WHITE);
-            canvass.drawRect(rect3, paint3);
-            paint3.setStyle(Paint.Style.STROKE);
-            paint3.setColor(Color.BLACK);
-            canvass.drawRect(rect3, paint3);
-            paint3.setTextSize(55);
-            paint3.setStyle(Paint.Style.FILL_AND_STROKE);
-            canvass.drawText("Make Your Own Design", 255, 650, paint3);
+            canvas.drawText("Play Your Game", 300, 1650, paint0);
 
 
 
@@ -143,5 +90,4 @@ public class DrawView2 extends View {
 
 
 }
-
 
